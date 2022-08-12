a460491caf3befdbe11744fe41af9f9d0848f652
riquelme <riquelmebandeira1@outlook.com>
2022-08-12 04:36:39 -0300

    Updates

---
+++ precache-manifest.b06a9adbe43fd2fdb3d918fe10923e97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96cde355f6f00389e32742972528193b",
+    "url": "/projeto-trivia/index.html"
+  },
+  {
+    "revision": "74466436368e9f02a387",
+    "url": "/projeto-trivia/static/css/main.482f8617.chunk.css"
+  },
+  {
+    "revision": "5e374fbc0004459d1bb9",
+    "url": "/projeto-trivia/static/js/2.c2bf54d1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/projeto-trivia/static/js/2.c2bf54d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74466436368e9f02a387",
+    "url": "/projeto-trivia/static/js/main.3d7e33e1.chunk.js"
+  },
+  {
+    "revision": "f379972a9cfb41901168",
+    "url": "/projeto-trivia/static/js/runtime-main.780537ca.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/projeto-trivia/static/media/trivia.466d153e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trivia/precache-manifest.7b7775422f07c8240cb231de2a118b8d.js"
+  "/projeto-trivia/precache-manifest.b06a9adbe43fd2fdb3d918fe10923e97.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trivia/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/projeto-trivia/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
